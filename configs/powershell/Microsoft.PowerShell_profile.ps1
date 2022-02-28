902c424bba12c6b68222d86d1dad62cd1fe7687e
BuriKizilkaya <burak.kizilkaya@outlook.com>
2022-02-28 19:45:03 +0100

    Refactor Theme of Powershell to robbyrussel

--- configs/powershell/Microsoft.PowerShell_profile.ps1
+++ configs/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 # -- oh my posh settings
-oh-my-posh --init --shell pwsh --config $HOME/AppData/Local/Programs/oh-my-posh/themes/tonybaloney.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config $HOME/AppData/Local/Programs/oh-my-posh/themes/robbyrussel.omp.json | Invoke-Expression
 
 # -- middlewares
 Import-Module -Name Terminal-Icons
